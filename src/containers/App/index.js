ea84a4c4ef3aac0679f29409e43fc3bfdf620f4d
vivihuang <rwhuang@thoughtworks.com>
2016-08-17 14:09:22 +0800

    Add header component to show page title.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
+import Header from '../../components/Header'
+
 const App = () => (
-  <div>My First App</div>
+  <div>
+    <Header />
+  </div>
 )
 
 export default App
