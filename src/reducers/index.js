8d874dc2e83f6a49434d7af17569c8f0db9ed029
vivihuang <rwhuang@thoughtworks.com>
2016-08-17 14:09:38 +0800

    Add todos reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { routerReducer as routing } from 'react-router-redux'
 import { combineReducers } from 'redux'
 
+import todos from './todos'
+
 export default combineReducers({
-  routing
+  routing,
+  todos
 })
---
+++ src/reducers/todos.js
@@ -0,0 +1,10 @@
+import { handleActions } from 'redux-actions'
+
+const initialState = [{
+  id: 0,
+  text: 'Use Redux'
+}]
+
+export default handleActions({
+
+}, initialState)
